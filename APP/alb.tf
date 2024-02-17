0dfec0fc93478ff6b1978119095a2f59ef77110f
Sudeep Saurabh <ssaurabh3@myseneca.ca>
2024-02-17 06:45:46 -0500

    Running Project

---
+++ APP/alb.tf
@@ -0,0 +1,44 @@
+resource "aws_lb" "alb" {
+  name               = "my-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = data.terraform_remote_state.network.outputs.public_subnet_id
+}
+
+resource "aws_lb_target_group" "lb_tg" {
+  name     = "my-tg"
+  port     = 8080
+  protocol = "HTTP"
+  target_type = "ip"  
+  vpc_id   = data.terraform_remote_state.network.outputs.vpc_id
+}
+
+resource "aws_lb_listener" "lb_listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.lb_tg.arn
+  }
+}
+
+resource "aws_security_group" "alb_sg" {
+  vpc_id = data.terraform_remote_state.network.outputs.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
